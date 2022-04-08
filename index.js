1d0c96f85f4a3935326d84808a8d83ac91fa6ea1
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-08 13:59:00 -0500

    commit tree 42c29d0048752f4df45f79e2e52e70f53265c22a

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials.map(x=>x.toUpperCase())
+// }
+
+// const titleCased = () => {
+//   return tutorials.map(x =>   x.charAt(0).toUpperCase()  )
+// }
+
+// const titleCased = () => {
+//   return tutorials.map(x =>   x.split(' ')  )
+// }
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(x => titleCaseSentence(x))
+  return nestedArrays
 }
+
+//split into 2 to reduce complexity a little bit.
+//take senence >> split into array of words >> map that array of words >> map function takes word and returns a string ${first letter capitalized} and rest of word using substring()}
+function titleCaseSentence(sentence){
+  return sentence.split(' ').map(x=> `${x[0].toUpperCase()}${x.substring(1,x.length)}`).join(' ')}
